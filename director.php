fce42e37f0ce6524be6de0eb6ae85499c6fc4b92
nhonnam <92206104+nhonnam@users.noreply.github.com>
2022-03-07 21:19:23 +0700

    ui commit

--- director.php
+++ director.php
@@ -1,34 +1,31 @@
 <head>
-                <meta http-equiv="refresh" content="<?php echo $_SESSION["refresh"];?>;URL='<?php echo $page?>'">
-                <form action="" method="post">
-                    <select name="refresh_time">
-                        <option value="">Stop refreshing</option>
-                        <option value=5>5 seconds</option>
-                        <option value=10>10 seconds</option>
-                        <option value=30>30 seconds</option>
-                    </select>
-                    <input type="submit" name="submit_time" value="Set time">
-                <label><?php echo "Current refresh time setting is ".$_SESSION["refresh"]." seconds.";?></label>
-                </form>
-            </head>
-
-            <form action="" method="post">
-            <select name="shop">
-                <option value="All_Shops">All Shops</option>
-                <option value="shop_a">Shop A</option>
-                <option value="shop_b">Shop B</option>
-            </select>
-            <input type="submit" name="selected_shop" value="Selected Shop">
-            </form>
-            <?php
-          
-                $selected_shop = $_SESSION["selected_shop"];
-                if($selected_shop=="All_Shops")
-                {
-                    $product_query = "SELECT * FROM product ORDER BY product_id";
-                }
-                else
-                {
-                    $product_query = "SELECT * FROM product WHERE shop = '$selected_shop' ORDER BY product_id";
-                }
-                ?>
+<meta http-equiv="refresh" content="<?php echo $_SESSION["refresh"];?>;URL='<?php echo $page?>'">
+    <form action="" method="post">
+        <select name="refresh_time">
+            <option value="">Stop refreshing</option>
+            <option value=5>5 seconds</option>
+            <option value=10>10 seconds</option>
+            <option value=30>30 seconds</option>
+        </select>
+        <input type="submit" name="submit_time" value="Set time">
+        <label><?php
+            if ($_SESSION["refresh"] != "")
+                echo "Current refresh time setting is ".$_SESSION["refresh"]." seconds.";?>
+        </label>
+    </form>
+</head>
+<form action="" method="post">
+    <select name="shop">
+        <option value="All_Shops">All Shops</option>
+        <option value="shop_a">Shop A</option>
+        <option value="shop_b">Shop B</option>
+    </select>
+    <input type="submit" name="selected_shop" value="Selected Shop">
+    </form>
+<?php
+    $selected_shop = $_SESSION["selected_shop"];
+    if($selected_shop=="All_Shops")
+        $product_query = "SELECT * FROM product ORDER BY product_id";
+    else
+        $product_query = "SELECT * FROM product WHERE shop = '$selected_shop' ORDER BY product_id";
+?>
--- index.php
+++ index.php
@@ -13,50 +13,51 @@
 	<link rel="stylesheet" href="index.css">
 </head>
 <body>
-	<header>
-		<div class="nav-bar">
-			<div><a class="nav" id="login" href="login.php">Log In to The Database</a></div>
-		</div>
-	</header>
-	<main>
-		<div class="logo_search-bar_menu">
-			<div><img class="logo" src="image/logo.png" alt="logo"><span>Toy Store<span></div>
-			<p>Welcome to Chain Stores of ATN!</p>
-		</div>
-		<div class="container"> 
-  			<div id="Carousel" class="carousel slide" data-ride="carousel">
-    			<ol class="carousel-indicators">
-      				<li data-slide-to="0" class="active"></li>
-      				<li data-slide-to="1"></li>
-      				<li data-slide-to="2"></li>
-    			</ol>
-    			<div class="carousel-inner">
-      				<div class="item active">
-        				<img class="slide" id="welcome" src="image/welcome.png" alt="Poster" style="width:100%;">
-      				</div>
-      				<div class="item">
-        				<img class="slide" id="to" src="image/to.png" alt="Poster" style="width:100%;">
-      				</div>    
-      				<div class="item">
-        				<img class="slide" id="atn" src="image/atn.png" alt="Poster" style="width:100%;">
-      				</div>
-    			</div>
-    			<a class="left carousel-control" id="left-control" href="#Carousel" data-slide="prev">
-      				<span class="glyphicon glyphicon-chevron-left"></span>
-   		 		</a>
-    			<a class="right carousel-control" id="right-control" href="#Carousel" data-slide="next">
-      				<span class="glyphicon glyphicon-chevron-right"></span>
-    			</a>
-  			</div>
-		</div>
-	</main>
-	<footer>
-		<p>Contact me on:</p>
-		<ul>
-			<li><img src="image/phone.png"></span> 0395453691</li>
-			<li><img src="image/facebook.png"><a id="link-fb" href="https://www.facebook.com/taotengivaybay" target="_blank"> Cậu Bé Vuôi Vẻ</a></li>
-			<li><span><img src="image/gmail.png"> namntkgcd201381@fpt.edu.vn</span></li>
-		</ul>
-		<p>Copyright &copy 2022 ATN Stores. All rights reserved.<p>
-	</footer>
+<header>
+	<div class="nav-bar">
+		<div><a class="nav" id="login" href="login.php">Log In to The Database</a></div>
+	</div>
+</header>
+<main>
+	<div class="logo_search-bar_menu">
+		<div><img class="logo" src="image/logo.png" alt="logo"><span>Toy Store<span></div>
+		<p>Welcome to Chain Stores of ATN!</p>
+	</div>
+	<div class="container"> 
+  		<div id="Carousel" class="carousel slide" data-ride="carousel">
+    		<ol class="carousel-indicators">
+      			<li data-slide-to="0" class="active"></li>
+      			<li data-slide-to="1"></li>
+      			<li data-slide-to="2"></li>
+    		</ol>
+    	<div class="carousel-inner">
+      		<div class="item active">
+        		<img class="slide" id="welcome" src="image/welcome.png" alt="Poster" style="width:100%;">
+      		</div>
+      		<div class="item">
+        		<img class="slide" id="to" src="image/to.png" alt="Poster" style="width:100%;">
+      		</div>    
+      		<div class="item">
+        		<img class="slide" id="atn" src="image/atn.png" alt="Poster" style="width:100%;">
+      		</div>
+    	</div>
+    	<a class="left carousel-control" id="left-control" href="#Carousel" data-slide="prev">
+      		<span class="glyphicon glyphicon-chevron-left"></span>
+   		</a>
+    	<a class="right carousel-control" id="right-control" href="#Carousel" data-slide="next">
+      		<span class="glyphicon glyphicon-chevron-right"></span>
+    	</a>
+  		</div>
+	</div>
+</main>
+<footer>
+	<p>Contact me on:</p>
+	<ul>
+		<li><img src="image/phone.png"></span> 0395453691</li>
+		<li><img src="image/facebook.png"><a id="link-fb" href="https://www.facebook.com/taotengivaybay" target="_blank"> Cậu Bé Vuôi Vẻ</a></li>
+		<li><span><img src="image/gmail.png"> namntkgcd201381@fpt.edu.vn</span></li>
+	</ul>
+	<p>Copyright &copy 2022 ATN Stores. All rights reserved.<p>
+</footer>
 </body>
+</html>
